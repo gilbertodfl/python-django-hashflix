f4cf67b1d0462350d3464f539e3be5fdb1355126
gilbertodfl <gilbertofl@tjdft.jus.br>
2026-02-20 17:39:32 -0300

    migrando FBS para CBS - Class-Based Views~

--- filme/urls.py
+++ filme/urls.py
@@ -1,8 +1,15 @@
 ## url - view - template
 from django.urls import path, include
-from .views import homepage
+from .views import  Homepage, Homefilmes
+
+### modelo FBS
+# urlpatterns = [
+#     path('', homepage),
+#     path('filmes/', homefilmes),
+# ]
+
+## modelo CBV  
 urlpatterns = [
-    path('', homepage),
-
+    path('', Homepage.as_view()),
+    path('filmes/', Homefilmes.as_view()),
 ]
-
--- filme/views.py
+++ filme/views.py
@@ -1,5 +1,27 @@
+from ast import List
 from django.shortcuts import render
+from .models import Filme
+from django.views.generic import TemplateView, ListView
 
-# Create your views here.
-def homepage(request):
-    return render(request, "homepage.html")
+
+## cbv é uma classe que herda de TemplateView, e tem um método get_context_data 
+# que é responsável por passar os dados para o template.
+class Homepage(TemplateView):
+    template_name = "homepage.html"
+    
+class Homefilmes(ListView):
+    template_name = "homefilmes.html"
+    model = Filme
+
+##FBS - Function Based Views. 
+## o exemplo abaixo é uma função que recebe a requisição do usuário, processa os dados e retorna uma resposta.
+
+#def homepage(request):
+#    return render(request, "homepage.html")
+
+# def homefilmes(request):
+#     context = {}
+#     lista_todos_filmes = Filme.objects.all()
+#     context ['lista_filmes'] = lista_todos_filmes
+#     return render(request, "homefilmes.html", context)
+
