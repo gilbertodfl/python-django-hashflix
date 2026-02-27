1a1d7df2932be7cc48717cb3d2a4cc71b9f041a3
gilbertodfl <gilbertofl@tjdft.jus.br>
2026-02-27 15:14:25 -0300

    Preparando arquivos para o RAILWAY.COM

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7i@jny5hv42*7v))zl9mnpm+)6s05u%8&o0$rww2)m+yfek_sh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -87,7 +88,15 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+import dj_database_url
+import os 
+DATABASES_URL = os.getenv('DATABASE_URL')
+if DATABASES_URL:
+    
+    DATABASES = {
+        'default' : dj_database_url.config(default=DATABASES_URL, conn_max_age=600)
+        }
+    
 ## Aqui estamos indicando que o modelo de usuário personalizado é o modelo Usuario que criamos no app filme, e que herda de AbstractUser.
 AUTH_USER_MODEL = 'filme.Usuario'
 
@@ -126,6 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+## aqui é a pasta onde os arquivos estáticos serão coletados quando rodarmos o comando collectstatic, 
+# veja o arquivo wsgi.py para mais detalhes.
+STATIC_ROOT =BASE_DIR /'staticfiles' 
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = 'media/'
